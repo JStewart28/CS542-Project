9f5e32340c3adfd34d4865db551d7952f26f96c2
Michael Servilla <miservilla@gmail.com>
2023-10-22 21:15:55 -0600

    renamed files, increased send size 512, moved files to code, added to .gitignore

--- code/all_to_all.cpp
+++ code/all_to_all.cpp
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <iostream>
 
+const int DATA_SIZE = 512;
+
 int main(int argc, char **argv)
 {
     MPI_Init(&argc, &argv);
@@ -11,17 +13,23 @@ int main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
-    int send_data = rank; // each process sends its own rank
-    int recv_data[num_procs];
+    char send_data[DATA_SIZE];
+    for (int i = 0; i < DATA_SIZE; i++)
+    {
+        send_data[i] = 1; 
+    }
+
+    char *recv_data = new char[num_procs * DATA_SIZE];
+
     double total_time = 0.0;
     int runs = 1000;
 
     for (int i = 0; i < runs; i++)
     {
-        MPI_Barrier(MPI_COMM_WORLD); 
+        MPI_Barrier(MPI_COMM_WORLD);
         double start_time = MPI_Wtime();
 
-        MPI_Alltoall(&send_data, 1, MPI_INT, recv_data, 1, MPI_INT, MPI_COMM_WORLD);
+        MPI_Alltoall(send_data, DATA_SIZE, MPI_CHAR, recv_data, DATA_SIZE, MPI_CHAR, MPI_COMM_WORLD);
 
         double end_time = MPI_Wtime();
         total_time += (end_time - start_time);
@@ -32,6 +40,8 @@ int main(int argc, char **argv)
         std::cout << "Average All-to-all time over " << runs << " runs: "
                   << total_time / runs << " seconds." << std::endl;
     }
+
+    delete[] recv_data;
 
     MPI_Finalize();
     return 0;
